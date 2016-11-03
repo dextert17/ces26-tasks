933d40647064a8d2a556f79a5e3cda5f66d2ae05
Mattheus Jucá <Mattheus Juc>
2016-11-03 15:16:48 -0200

    Tarefa da Semana 6

--- scripts/semana6.js
+++
@@ -1,8 +0,0 @@
-System.config({
-  transpiler: 'typescript',
-  typescriptOptions: { emitDecoratorMetadata: true },
-  packages: {'app': {defaultExtension: 'ts'}}
-});
-
-System.import('/angular2/src/app/hello_world_main')
-	.then(null, console.error.bind(console));
